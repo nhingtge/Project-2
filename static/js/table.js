84a889338ba840a0aa42a5b2b25a814e020a21ff
Falcon <65928199+Falcon-d@users.noreply.github.com>
2020-10-19 21:18:41 -0700

    Add files via upload

--- static/js/table.js
+++ static/js/table.js
@@ -1,10 +1,23 @@
-$(document).ready(function() {
-    $('#example').DataTable( {
-        "initComplete": function () {
-            var api = this.api();
-            api.$('td').click( function () {
-                api.search( this.innerHTML ).draw();
-            } );
-        }
+const url = "/api/datatable";
+  d3.json(url).then(function(response) {
+    dataSet=response["results"]
+    $(document).ready(function() {
+        $('#myTable').DataTable( {
+            data: dataSet,
+            columns: [
+                { title: "Leauge" },
+                { title: "Season" },
+                { title: "Stage" },
+                { title: "Player" },
+                { title: "Team" },
+                { title: "Games Played" },
+                { title: "Minutes Played" },
+                { title: "Field Goals" },
+                { title: "Rebounds" },
+                { title: "Assists" }
+
+            ]
+        } );
     } );
-} );
+    console.log(response);
+  })
