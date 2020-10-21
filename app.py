d674b65434b32ff79e62b73faa5d9614524ba7c7
mitchellmf <38438850+mitchellmf@users.noreply.github.com>
2020-10-21 14:35:02 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -52,28 +52,9 @@ def datatable():
     session = Session(engine)
     datatable=[]
     results = session.query(Bball.League,Bball.Season,Bball.Stage,Bball.League,Bball.Team,Bball.GP,Bball.MIN,Bball.FGA,Bball.REB,Bball.AST).all()
-    league = [result[0] for result in results]
-    datatable.append(league)
-    season = [result[1] for result in results]
-    datatable.append(season)
-    stage = [result[2] for result in results]
-    datatable.append(stage)
-    player = [result[3] for result in results]
-    datatable.append(player)
-    team = [result[4] for result in results]
-    datatable.append(team)
-    games_played = [result[5] for result in results]
-    datatable.append(games_played)
-    mins_played = [result[6] for result in results]
-    datatable.append(mins_played)
-    field_goals = [result[7] for result in results]
-    datatable.append(field_goals)
-    rebounds = [result[8] for result in results]
-    datatable.append(rebounds)
-    assists = [result[9] for result in results]
-    datatable.append(assists)
+    datatable = [list(element) for element in results]
     dataresults={"results": datatable}
-    return jsonify (dataresults) 
+    return jsonify (dataresults)
 
 # Query the database and send the jsonified results
 @app.route("/api/bball")
